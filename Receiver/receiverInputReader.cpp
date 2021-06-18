1328ed7dddd2fd7bfa3cfb7e447ce375ce7377f3
virendra <16984895+virendrasjhala@users.noreply.github.com>
2021-06-18 16:04:16 +0530

    Update receiverInputReader.cpp

--- Receiver/receiverInputReader.cpp
+++ Receiver/receiverInputReader.cpp
@@ -1,24 +1,20 @@
-//#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_MAIN
 #include"catch.hpp"
-#include"CaptureData.h"
-#include<string>
+#include "CaptureData.h"
+#include <string>
 #include<iostream>
 #include<vector>
 #include<regex>
 
-int main(int argc, char** argv)
-{
+
+TEST_CASE("Reding input from sender") {
 	std::string input;;
 	ReadingData data;
-	std::cout << "You have entered " << argc
-		<< " arguments:" << "\n";
-	for (int i = 0; i < argc; ++i){
-		std::cout << argv[i] << "\n";
-	//while (std::getline(std::cin, input))
-	//{
-		//if (input.empty())
-		//	break;
-		input = argv[i];
+	while (std::getline(std::cin, input))
+	{
+		if (input.empty())
+			break;
+
 		std::regex reg("-?(([1-9]\\d*\\.\\d*)|(0\\.\\d*[1-9]\\d*))");
 		const std::sregex_iterator end;
 		for (std::sregex_iterator iter(cbegin(input), std::cend(input), reg); iter != end; ++iter) {
@@ -29,8 +25,7 @@ int main(int argc, char** argv)
 		data.readingData.push_back(data.temp);
 		data.temp.clear();
 	}
-	
-	
+
 	/*------------------------------separate data------------------------------------*/
 	data.seperateData(data.readingData);
 	
@@ -50,6 +45,4 @@ int main(int argc, char** argv)
 	assert(data.refresh() == true);
 	
 	system("pause");
-
-	return 0;
 }
